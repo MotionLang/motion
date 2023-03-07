ffb116fdea968748a73c8c7bdd8b6ec024426bde
UPWRD <96660898+UPWRD1@users.noreply.github.com>
2023-03-07 19:38:15 +0000

    formats, strings

--- include/object.h
+++ include/object.h
@@ -8,8 +8,8 @@
 
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
 
-#define AS_STRING(value) ((ObjString *)AS_OBJ(value))
-#define AS_CSTRING(value) (((ObjString *)AS_OBJ(value))->chars)
+#define AS_STRING(value) ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
     OBJ_STRING,
@@ -17,18 +17,18 @@ typedef enum {
 
 struct Obj {
     ObjType type;
-    struct Obj *next;
+    struct Obj* next;
 };
 
 struct ObjString {
     Obj obj;
     int length;
-    char *chars;
+    char* chars;
     uint32_t hash;
 };
 
-ObjString *takeString(char *chars, int length);
-ObjString *copyString(const char *chars, int length);
+ObjString* takeString(char* chars, int length);
+ObjString* copyString(const char* chars, int length);
 void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type) {
--- src/object.c
+++ src/object.c
@@ -66,7 +66,7 @@ ObjString* copyString(const char* chars, int length) {
 void printObject(Value value) {
     switch (OBJ_TYPE(value)) {
         case OBJ_STRING:
-            a printf("%s", AS_CSTRING(value));
+            printf("%s", AS_CSTRING(value));
             break;
     }
 }
